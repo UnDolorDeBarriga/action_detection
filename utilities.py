97b2b885385249a721a44a736af3624626e09ae5
Er frittata <giacomo.montagna1@gmail.com>
2025-05-09 17:12:06 +0200

    bruh

--- utilities.py
+++ utilities.py
@@ -351,7 +351,7 @@ def extract_keypots_cordinates(results) -> tuple:
     """
     if results.pose_landmarks:
         pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark])
-        pose[23:] = np.zeros((10, 4))  
+       # pose[23:] = np.zeros((10, 4))  
     else:
         pose = np.zeros((33, 4))
     # face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
